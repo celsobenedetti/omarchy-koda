7d5ae715c02fa8c6b84c31e1144935a7b631da15
Celso Benedetti <celsobenedetti2@gmail.com>
2026-01-14 15:40:09 -0300

    feat: nvim theme

--- neovim.lua
+++ neovim.lua
@@ -1,19 +1,13 @@
 return {
 	{
-		"everviolet/nvim",
-		name = "evergarden",
-		opts = {
-			theme = {
-				variant = "fall",
-				accent = "skye",
-			},
-		},
+		"oskarnurm/koda.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
 	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "evergarden",
-			background = "soft",
+			colorscheme = "koda",
 		},
 	},
 }
